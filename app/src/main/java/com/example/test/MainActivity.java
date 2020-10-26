4af5028d8489dea43b773f71e63b518366e05ced
bam4wo <43362974+bam4wo@users.noreply.github.com>
2020-10-26 22:27:05 +0800

    1026

--- app/src/main/java/com/example/test/MainActivity.java
+++ app/src/main/java/com/example/test/MainActivity.java
@@ -3,12 +3,33 @@ package com.example.test;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private EditText edMail;
+    private EditText edPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edMail = findViewById(R.id.ed_email);
+        edPassword = findViewById(R.id.ed_password);
+        Button button = findViewById(R.id.login);
+
     }
+
+    @Override
+    public void onClick(View v) {
+        //Thread thread = new Thread(mutiThread);
+        //thread.start();
+    }
+
 }
---
+++ app/src/main/java/com/example/test/MysqlCon.java
@@ -0,0 +1,11 @@
+package com.example.test;
+import android.util.Log;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class MysqlCon {
+}
